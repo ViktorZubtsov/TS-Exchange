5248a8166c6f04c8c28435cb208579b006e5eff1
zubtsov <zubtsov_viktor@smartworld.team>
2021-03-26 18:55:22 +0700

    Add  Goal class

--- src/classes/Goal/Goal.ts
+++ src/classes/Goal/Goal.ts
@@ -1,10 +1,33 @@
 import {ITypeGoal} from "../../interfaces/ITypeGoal";
 
 export class Goal {
+    goals: ITypeGoal[]
 
+    constructor(goals: ITypeGoal[]) {
+        this.goals = goals
+    }
+    public getG(): Array<object> {
+        return this.goals
+    }
 }
 
 
 class TypeGoal implements ITypeGoal{
-    id: 1,
+    id: number
+    name: string
+
+    constructor(name: string) {
+        this.name = name
+        this.id = this.getRandomInt()
+    }
+    getRandomInt(): number {
+        return Math.floor(Math.random() * (1 - 10)) + 1;
+    }
 }
+
+
+
+const a = new TypeGoal('dweqdwdw')
+const b = new TypeGoal('dweqdwdwqwqewq333ewqdfeww')
+
+export const sa = new Goal([a,b])
