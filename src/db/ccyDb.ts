2bef45733c294325b5906f95ce25e54e00606a87
ViktorZubtsov <mr.viktor.1996@yandex.ru>
2021-04-25 21:22:21 +0700

    Add ccy class

---
+++ src/db/ccyDb.ts
@@ -0,0 +1,8 @@
+import {ICCY} from "../interfaces/CCY/ICCY";
+
+export const ccyDb: Array<ICCY> = [
+    {id: 1, name: 'Bitcoin', shortName: 'BTC', numberSign: 8},
+    {id: 2, name: 'Umi', shortName: 'UMI', numberSign: 4},
+    {id: 3, name: 'PRIZM', shortName: 'PZM', numberSign: 4},
+    {id: 4, name: 'Alti', shortName: 'ALT', numberSign: 5},
+];
