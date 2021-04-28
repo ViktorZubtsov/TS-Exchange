7faa17853144910bff5085a26f9553c2d31276d3
zubtsov <zubtsov_viktor@smartworld.team>
2021-04-28 18:09:21 +0700

    Edit doc

--- index.ts
+++ index.ts
@@ -3,6 +3,5 @@ import {UserExchange} from "./src/classes/UserExchange";
 
 const viktor = new UserExchange('mr.viktor.1996.@yandex.ru', '1111');
 
-viktor.setPass('11ss11', 'fgfxxgfgf');
-console.log(viktor.getPass());
+console.log(viktor);
 
