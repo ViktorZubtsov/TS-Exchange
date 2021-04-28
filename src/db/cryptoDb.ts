a2ca496c685dc2e8854a00b0050d11f6ca3b2934
zubtsov <zubtsov_viktor@smartworld.team>
2021-04-28 18:47:48 +0700

    Add cryptoDb

---
+++ src/db/cryptoDb.ts
@@ -0,0 +1,8 @@
+import {ICtypro} from "../interfaces/Crypto/ICtypro";
+
+export const cryptoDb: Array<ICtypro> = [
+    {id: 1, ccyId: 1, balance: 1000 },
+    {id: 2, ccyId: 2, balance: 56000 },
+    {id: 3, ccyId: 3, balance: 1532 },
+    {id: 4, ccyId: 4, balance: 84584 }
+];
